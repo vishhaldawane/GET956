dd68987087940b856b273a207249e1e6830ed608
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2021-08-30 11:25:24 +0530

    Add files via upload

--- LambdaTest.java
+++ LambdaTest.java
@@ -26,10 +26,14 @@ class Violin implements MusicalInstrument {
 	public void bowing() {
 		System.out.println("Bowing Violin....");
 	}
-}*/
-class Tree // - Tree.class
+}
+class Cello extends Violin {
+
+}
+*/
+class Tree // - Tree.class - outer class only be public, abstract or final
 {
-	class BirdNest //named inner class -Tree$BirdNest.class
+	private class BirdNest //named inner class -Tree$BirdNest.class
 	{
 		
 	}
@@ -38,23 +42,67 @@ class Tree // - Tree.class
 	
 }
 
+abstract class BankAccount { 
+	private int bankAccount;
+	
+} //meant for inheritance
+final class Chess {} //cannot be extended
+
+//The class BankChess can be either abstract or final, not both
+//abstract final class BankChess { }
+
+class UseAnnonymousInnerClass
+{
+	static void callMeIMAnnonymous(MusicalInstrument x) {
+		System.out.println("Annonymous...started");
+		x.play();
+		System.out.println("Annonymous...over");
+	}
+}
+interface A {
+	void foo(); //promise or mandate of its implementation
+}
 public class LambdaTest {
 	
+	
+	void fun() {
+		
+		
+	/*class A*/	
+		A a = ()-> 
+		{
+			System.out.println("lambda function invoked...");	
+			
+		};
+		//A a = new A();
+		a.foo();
+	}
+	
+	
 	public static void main(String[] args) {
+		LambdaTest lt = new LambdaTest();
+		lt.fun();
+			//A a = new A();
 			
 		//MusicalInstrument mi = new Violin(); //loose coupling 
 		//mi.play();
 		//System.out.println("-------");
 		
+		//Guitar is named class, and below in unNAMED class
+		
 		MusicalInstrument mi = new MusicalInstrument() //LambdaTest$1.class
-		{
+		{ //child class of MusicalInstrument interface
 			public void play() {
 					System.out.println("Playing unnamed class... plucking/playing a Guitar...");
 			}
+			public void fun() { }
 		}; //<-- look at the semicolon  here
 		mi.play();
 		
-		MusicalInstrument mi2 = new MusicalInstrument() //LambdaTest$1.class
+		UseAnnonymousInnerClass.callMeIMAnnonymous(mi);
+		//mi.fun();
+		
+		MusicalInstrument mi2 = new MusicalInstrument() //LambdaTest$2.class
 				{
 					public void play() {
 							System.out.println("Playing unnamed class... tinging/playing a Sitar...");
@@ -63,7 +111,7 @@ public class LambdaTest {
 				mi2.play();
 				
 				
-				MusicalInstrument mi3 = new MusicalInstrument() //LambdaTest$1.class
+				MusicalInstrument mi3 = new MusicalInstrument() //LambdaTest$3.class
 						{
 							public void play() {
 									System.out.println("Playing unnamed class... bowing/playing a Violin...");
@@ -71,7 +119,9 @@ public class LambdaTest {
 						}; //<-- look at the semicolon  here
 						mi3.play();
 						
-				MusicalInstrument	mi4	= () -> {
+		//is it as if a function of the LambdaTest.java file 
+				MusicalInstrument	mi4	= () -> 
+				{
 					System.out.println("Playing lambda function... Playing Veena...");
 					
 				};
